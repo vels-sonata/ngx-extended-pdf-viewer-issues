8caba445fb4d6370a6ec9fbb6020ca7446a29a44
stephan <webmaster1@beyondjava.de>
2020-07-05 22:43:41 +0200

    cosmetics

--- oauth2-ngx-extended-pdf-viewer-test/src/main/webapp/app/ngx-extended-pdf/ngx-extended-pdf-test/ngx-extended-pdf-test.component.ts
+++ oauth2-ngx-extended-pdf-viewer-test/src/main/webapp/app/ngx-extended-pdf/ngx-extended-pdf-test/ngx-extended-pdf-test.component.ts
@@ -1,20 +1,17 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { KeycloakService } from '../../oauth2/KeycloakService';
 
 @Component({
-    selector: 'jhi-ngx-extended-pdf-test',
-    templateUrl: './ngx-extended-pdf-test.component.html',
-    styleUrls: ['./ngx-extended-pdf-test.component.scss']
+  selector: 'jhi-ngx-extended-pdf-test',
+  templateUrl: './ngx-extended-pdf-test.component.html',
+  styleUrls: ['./ngx-extended-pdf-test.component.scss'],
 })
 export class NgxExtendedPdfTestComponent implements OnInit {
-
   public bearerToken: string | undefined = undefined;
 
-    constructor(private keycloakService: KeycloakService) {
-    }
+  constructor(private keycloakService: KeycloakService) {}
 
-    ngOnInit(): void {
-      this.bearerToken = "Bearer " + this.keycloakService.getToken();
-    }
-
+  ngOnInit(): void {
+    this.bearerToken = 'Bearer ' + this.keycloakService.getToken();
+  }
 }
